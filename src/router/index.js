faca201074d1768d20cb4e577d4b913c532fb76e
Clara MOUZANNAR <claramouzannar@mcbookc.home>
2024-10-09 18:12:20 +0200

    Added homepage component and updated routing for homepage in add-country-field branch

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Skull from '../components/Skull.vue'
 import AppAccounts from '../components/AppAccounts.vue'
+import Homepage from '../components/Homepage.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Homepage',
+    component: Homepage   // Add the route for Homepage
+  },
   {
     path: '/skull',
     name: 'Skull',
